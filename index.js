d1195f01c16a119b53c5534ef8cc2bb899ea9457
Baldeep Singh <itsbaldeep@gmail.com>
2020-01-16 22:02:46 +0530

    main commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const Discord = require('discord.js');
+const https = require('https');
+const prefix = "send ";
+
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    client.user.setActivity("with eggplant", "PLAYING");
+});
+
+client.on('message', async message => {
+    if (message.author.bot) return;
+    if (message.content.indexOf(prefix) !== 0) return;
+
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const input = args.shift().toLowerCase();
+
+    const photo_count = 30;
+    const options = {
+        host: 'api.pexels.com',
+        path: `/v1/search?per_page=${photo_count}&query=${input}`,
+        headers: { 'Authorization': process.env.PEXELS_API }
+    };
+    const req = https.request(options, res => {
+        let buffer = '';
+        res.on('data', res => buffer += res);
+        res.on('end', () => {
+            const data = JSON.parse(buffer);
+            if (data.photos.length == 0) return;
+            const index = Math.floor(Math.random() * photo_count);
+            const url = data.photos[index].src.original;
+            message.reply(url);
+        })
+    });
+    req.end();
+});
+
+client.login(process.env.DISCORD_API);
