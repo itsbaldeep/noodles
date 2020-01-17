843b4bb9a0f2f3f6fabd9e6ff104dc71eefcd2cb
Baldeep Singh <itsbaldeep@gmail.com>
2020-01-17 21:13:40 +0530

    finalizing

--- index.js
+++ index.js
@@ -12,18 +12,24 @@ client.on('message', async message => {
     if (message.author.bot) return;
     if (message.content.toLowerCase().indexOf(prefix) !== 0) return;
 
+    // Getting the arguments
     const args = message.content.slice(prefix.length).trim().split(/ +/g);
     const input = args.join('%20').toLowerCase();
-    console.log(args, input);
 
+    // Options for API call
     const options = {
         host: 'api.pexels.com',
         path: `/v1/search?per_page=80&query=${input}`,
         headers: { 'Authorization': process.env.PEXELS_API }
     };
+
+    // Making request
     const req = https.request(options, res => {
+        // Getting the data
         let buffer = '';
         res.on('data', res => buffer += res);
+
+        // Sending the message
         res.on('end', () => {
             const data = JSON.parse(buffer);
             const length = Math.min(data.per_page, data.total_results);
@@ -35,8 +41,11 @@ client.on('message', async message => {
             else
                 message.channel.send(':man_gesturing_no: No photos found relating to your search!');
         });
+
+        // Checking errors
         res.on('error', console.log);
     });
+
     req.end();
 });
 
