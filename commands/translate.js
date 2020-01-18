d8cfd13ee4f96eeb94f5958b556612df8f0a05a2
Baldeep Singh <itsbaldeep@gmail.com>
2020-01-18 20:08:06 +0530

    added translation command

--- commands/translate.js
+++ commands/translate.js
@@ -1,6 +1,26 @@
 module.exports = {
     name: 'translate',
     execute(message, args) {
-        message.channel.send("translate!");
+        const [sl, tl, query] = args;
+        if (!sl || !tl || !query) return;
+        
+        const https = require('https');
+        const options = {
+            host: 'translate.googleapis.com',
+            path: `/translate_a/single?client=gtx&sl=${sl}&tl=${tl}&dt=t&q=${query}`
+        };
+
+        const req = https.request(options, res => {
+            let buffer = '';
+            res.on('data', res => buffer += res);
+            res.on('end', () => {
+                const data = JSON.parse(buffer);
+                const text = data[0][0][0];
+
+                message.channel.send(text);
+            });
+            res.on('err', console.error);
+        });
+        req.end();
     }
 }
