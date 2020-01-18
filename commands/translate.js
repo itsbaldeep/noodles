ae6d27fd05a92a89fb98e7c4e80dc48d880ce399
Baldeep Singh <itsbaldeep@gmail.com>
2020-01-18 19:54:44 +0530

    attempt at implementing dynamic commands

---
+++ commands/translate.js
@@ -0,0 +1,6 @@
+module.exports = {
+    name: 'translate',
+    execute(message, args) {
+        message.channel.send("translate!");
+    }
+}
--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const Discord = require('discord.js');
-const https = require('https');
-const { prefix } = require('./config.json');
+const fs = require('fs');
+const { commands } = require('./config.json');
 
 // Making the client
 const client = new Discord.Client();
+client.commands = new Discord.Collection();
+
+// Getting commands
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.name, command);
+}
 
 // Handling ready event
 client.on('ready', () => {
@@ -13,43 +21,31 @@ client.on('ready', () => {
 // Handling message event
 client.on('message', message => {
     // Exit early
-    if (!message.content.startsWith(prefix) || message.author.bot) return;
+    if (message.author.bot) return;
 
-    // Getting the arguments
-    const args = message.content.slice(prefix.length).split(/ +/);
-    const input = args.join('%20').toLowerCase();
+    // Getting command name
+    let commandName;
+    for (const c of commands) {
+        if (message.content.startsWith(c)) {
+            commandName = c;
+            break;
+        }
+    }
+    if (!commandName) return;
 
-    // Options for API call
-    const options = {
-        host: 'api.pexels.com',
-        path: `/v1/search?per_page=80&query=${input}`,
-        headers: { 'Authorization': process.env.PEXELS_API }
-    };
+    // Getting arguments
+    const args = message.content.splice(commandName.length).split(/ +/);
 
-    // Making request
-    const req = https.request(options, res => {
-        // Getting the data
-        let buffer = '';
-        res.on('data', res => buffer += res);
+    // Getting command
+    const command = client.commands.get(commandName);
+    if (!command) return;
 
-        // Sending the message
-        res.on('end', () => {
-            const data = JSON.parse(buffer);
-            const length = Math.min(data.per_page, data.total_results);
-            const index = Math.floor(Math.random() * length);
-            const photo = data.photos[index];
-
-            if (photo) 
-                message.channel.send(`:man_gesturing_ok: ${args.join(' ')} for you! ${photo.src.original}`);
-            else
-                message.channel.send(':man_gesturing_no: No photos found relating to your search!');
-        });
-
-        // Checking errors
-        res.on('error', console.log);
-    });
-
-    req.end();
+    // Executing command
+    try {
+		command.execute(message, args);
+	} catch (error) {
+        console.error(error);
+    }
 });
 
 // Starting the client;
